75024bebf48f7b3af5defe0f3cc63e8703571d57
Joaqo <joaqo9909@gmail.com>
2026-05-16 16:10:20 -0300

    FSI 2

--- src/room_modal_optimizer/evaluation/evaluator.py
+++ src/room_modal_optimizer/evaluation/evaluator.py
@@ -3,15 +3,16 @@ class Evaluator:
         ...
 
     def fsi(f_modes, n):
-    """
-    Frequency Spacing Index
-    Inputs:
-        - f_modes: array type object. modes frequencies vector
-        - n: int type object. Max mode to evaluate.
-    """
-    f = f_modes[:n]
-    avg_frq_sp = (f[-1]-f[0])/(n-1)
-    dif_nhb_modes = np.diff(f)
-    sum_arg = (dif_nhb_modes/avg_frq_sp)**2
-    frq_sp_idx = (1/(n-1))*np.sum(sum_arg, axis=0)
-    return frq_sp_idx
+        """
+        Frequency Spacing Index
+        Inputs:
+            - f_modes: array type object. modes frequencies vector
+            - n: int type object. Max mode to evaluate.
+        """
+        
+        f = f_modes[:n]
+        avg_frq_sp = (f[-1]-f[0])/(n-1)
+        dif_nhb_modes = np.diff(f)
+        sum_arg = (dif_nhb_modes/avg_frq_sp)**2
+        frq_sp_idx = (1/(n-1))*np.sum(sum_arg, axis=0)
+        return frq_sp_idx
